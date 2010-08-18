66eb6984e3ab15b434c1f3ca67c9b8696ea014b1
John McAliley <john@couponshack.com>
2010-08-18 16:04:59 -0400

    removed client from app controller.  Engine will load this automatically

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   layout 'application'
-  
-  
-  protected
-
-  def client
-    @client ||= FBGraph::Client.new(:client_id => '114709451898747',
-                                   :secret_id => '7c87164749a02552e3d204142e3af16b' , 
-                                   :token => session[:access_token])
-  end
 end
